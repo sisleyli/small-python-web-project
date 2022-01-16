33b3df1787773ba5aa22cd64e36bd6c1ea3981f3
sisleyli <1422162310@qq.com>
2022-01-16 07:30:21 +0000

    create vue framework

---
+++ frontend/onnx-manage/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="">
+    <onnx-manage/>
+  </div>
+</template>
+
+<script>
+import OnnxManage from './components/OnnxManage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    OnnxManage
+  }
+}
+</script>
+
+<style scoped>
+.whole-container{
+  height: 100vh;
+  width: 100vw;
+}
+</style>
